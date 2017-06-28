d11a9c1a6ad76ef2242199cbe08c463424aa8cec
Sara Schultz <saschultz8@gmail.com>
2017-06-28 13:46:29 -0700

    pass spec for setting default rating score after work creation

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,9 @@
 class Work < ActiveRecord::Base
   validates(:artist, :location, :link, :presence => true)
+  after_create(:default_rating)
+
+  def default_rating
+    self.rating = 0
+  end
+  
 end
--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -4,4 +4,6 @@ describe Work do
   it { should validate_presence_of :artist }
   it { should validate_presence_of :location }
   it { should validate_presence_of :link }
+  it { is_expected.to callback(:default_rating).after(:create) }
+
 end
