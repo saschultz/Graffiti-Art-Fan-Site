c64e4a896f3a1404bcfd200078c0e9e71c2b9b28
Sara Schultz <saschultz8@gmail.com>
2017-06-28 14:56:22 -0700

    add create work functionality

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,4 +6,24 @@ class WorksController < ApplicationController
   def show
     @work = Work.find(params[:id])
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    # @work.score = 0
+     if @work.save
+       redirect_to works_path
+     else
+       render :new
+     end
+  end
+
+private
+  def work_params
+    params.require(:work).permit(:artist, :location, :link)
+  end
+
 end
